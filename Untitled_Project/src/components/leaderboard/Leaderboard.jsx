e70d558eecb0120aa97ef445ad639dbb277a6af6
djk239 <dale.king@rutgers.edu>
2024-06-25 21:31:29 -0400

    small leaderboard update

--- Untitled_Project/src/components/leaderboard/Leaderboard.jsx
+++ Untitled_Project/src/components/leaderboard/Leaderboard.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import styles from './Leaderboard.module.css'
 import { fetchMyScore, fetchTopScores } from '../../api'
 
 export default function Leaderboard({isLoggedIn, handleLog, handleLogout}) {
+
+    const [leaderboard, setLeaderboard] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchLeaderboard = async () => {
+            try {
+                const response = await fetchTopScores();    
+                const data = await response.json();
+                setLeaderboard(data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        fetchLeaderboard();
+    }, []);
+
+
     console.log(typeof handleLog)
     const loadtop10 = async () => {
         const clip = await fetchTopScores();
