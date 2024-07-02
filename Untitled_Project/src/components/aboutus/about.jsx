ed72a25d0f3dc15a100bada2671ef28dafe4911c
Darryon <darryonrivera@gmail.com>
2024-07-01 23:54:41 -0400

    refactored aboutus page

--- Untitled_Project/src/components/aboutus/about.jsx
+++ Untitled_Project/src/components/aboutus/about.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import './aboutus.css'; // Import the CSS file
+import styles from './aboutus.module.css'; // Import the CSS file
 import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
   return (
-    <div className="container">
-      <div className="heading">
+    <div className={styles.container}>
+      <div className={styles.heading}>
         <h1>About Us</h1>
         <p>.Non quia facilis est, sed quia hard.</p>
       </div>
-      <section className="about">
-        <div className="about-image">
+      <section className={styles.about}>
+        <div className={styles.aboutimage}>
           <img src="./public/images/cartoonguys.png" alt="Image" />
         </div>
-        <div className="about-content">
+        <div className={styles.aboutcontent}>
           {/* Additional content goes here */}
           <h2>Welcome User</h2>
           <p>This is the story...</p>
-          <Link to={'/'} className="homelink">Home</Link>
+          <Link to={'/'} className={styles.homelink}>Home</Link>
         </div>
       </section>
     </div>
