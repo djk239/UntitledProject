535aa5a7d814c6e4457e8a7593391ac96285a8a5
Darryon <darryonrivera@gmail.com>
2024-07-06 11:26:32 -0400

    created game card modules; changed homepage background to temp blue/green/black;

---
+++ Untitled_Project/src/components/homepage/homegamecard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './homegamecard.module.css'
+const Homegamecard = () => {
+  return (
+    <div>
+        <Link to={'/melodymystery'} className= {`${styles.card} ${styles.card1} ${styles.transparent1}`}>
+            <h5>Play Game</h5>
+        </Link>
+        <div className= {`${styles.card} ${styles.card2} ${styles.transparent2}`}>
+            <h5>Coming Soon</h5>
+        </div>
+        <div className={`${styles.card} ${styles.card3} ${styles.transparent3}`}>
+            <h5>Coming Soon</h5>
+        </div>
+        <div className={`${styles.card} ${styles.card4} ${styles.transparent4}`}>
+            <h5>Coming Soon</h5>
+        </div>
+    </div>
+  )
+}
+
+export default Homegamecard
--- Untitled_Project/src/components/homepage/homepage.jsx
+++ Untitled_Project/src/components/homepage/homepage.jsx
@@ -1,40 +1,39 @@
 import React from 'react';
 import styles from './homestyle.module.css';
-import { Link } from 'react-router-dom';
-import HomeNav from './homenav'
+import HomeNav from './homenav';
+import style from './homepage.module.css';
+import Homegamecard from './homegamecard';
 
 const HomePage = () => {
   return (
+    <div className= {style.App}>
+
     <div className= {styles.container}>
       <div className= {styles.navbar}>
         <div>
           <HomeNav />
         </div>
         <div className= {styles.transparentlogo}>
-          <img src="./public/images/logo2.png" className= {styles.logo} alt="Logo" />
+          <img src="./images/logo2.png" className= {styles.logo} alt="Logo" />
         </div>
       </div>
+
+      <div className= {style.content}>
+
       <div className= {styles.row}>
         <div className= {styles.col}>
           <h1 className= {styles.maintitleheader}>Melody Mystery</h1>
           <p>Welcome to Melody Mystery, the ultimate music guessing game! Test your music knowledge and see how well you know your favorite tunes. In this thrilling game, you'll be presented with short clips from a wide variety of songs. Your challenge is to guess the song title and artist as quickly as possible. The faster you guess correctly, the higher your score!</p>
           <button type="button" className= {styles.button}>Sign In</button>
         </div>
+        
         <div className={styles.col}>
-          <Link to={'/melodymystery'} className= {`${styles.card} ${styles.card1} ${styles.transparent1}`}>
-            <h5>Play Game</h5>
-          </Link>
-          <div className= {`${styles.card} ${styles.card2} ${styles.transparent2}`}>
-            <h5>Coming Soon</h5>
-          </div>
-          <div className={`${styles.card} ${styles.card3} ${styles.transparent3}`}>
-            <h5>Coming Soon</h5>
-          </div>
-          <div className={`${styles.card} ${styles.card4} ${styles.transparent4}`}>
-            <h5>Coming Soon</h5>
-          </div>
+          <Homegamecard />
         </div>
       </div>
+
+      </div>
+    </div>
     </div>
   );
 };
