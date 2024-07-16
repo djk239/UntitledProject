df4ff0efd9e671e6f84f4f977f2f1f6c338e7209
Darryon <darryonrivera@gmail.com>
2024-07-15 23:24:52 -0400

    finished presentable home page; request merge

--- Untitled_Project/src/components/homepage/homegamecard.jsx
+++ Untitled_Project/src/components/homepage/homegamecard.jsx
@@ -5,22 +5,22 @@ const Homegamecard = () => {
   return (
     <div>
         <Link to={'/melodymystery'} className= {`${styles.card} ${styles.card1} ${styles.transparent1}`}>
-            <h5>Play Game</h5>
+            <h5></h5>
         </Link>
         <div className= {`${styles.card} ${styles.card2} ${styles.transparent2}`}>
-            <h5>Coming Soon</h5>
+            <h5></h5>
         </div>
         <div className={`${styles.card} ${styles.card3} ${styles.transparent3}`}>
-            <h5>Coming Soon</h5>
+            <h5></h5>
         </div>
         <div className={`${styles.card} ${styles.card4} ${styles.transparent4}`}>
-            <h5>Coming Soon</h5>
+            <h5></h5>
         </div>
         <div className={`${styles.card} ${styles.card5} ${styles.transparent5}`}>
-            <h5>Coming Soon</h5>
+            <h5></h5>
         </div>
         <div className={`${styles.card} ${styles.card6} ${styles.transparent6}`}>
-            <h5>Coming Soon</h5>
+            <h5></h5>
         </div>
         
     </div>
